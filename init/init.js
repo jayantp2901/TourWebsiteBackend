479ee6b88ba9a49b60019b4bd09394112585eb19
jayantp@gmail.com <jayantp8767@gmail.com>
2024-12-27 11:30:37 +0530

    updated init

--- init/init.js
+++ init/init.js
@@ -1,11 +1,17 @@
 import mongoose from 'mongoose';
 import { tourOptions } from './data.js';  // Correct import path with .js extension
 import { TourOption } from '../models/tourSchema.js';  // Correct relative path and extension
+import dotenv from 'dotenv';
+dotenv.config();
 
+
+const MONGO_URL="mongodb+srv://jayantpaithane111:pt4UoH4AlLRUNAn1@tourwebsite.sdowz.mongodb.net/tourwebsite?retryWrites=true&w=majority"
+
+console.log("Mongo URL ",MONGO_URL)
 const insertData = async () => {
   try {
     // Connect to MongoDB
-    await mongoose.connect('mongodb://localhost:27017/tourwebsite', {
+    await mongoose.connect(MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
